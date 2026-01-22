c09f82fa5dbf2220ed726e47e7a7bb0cc271f5ab
zeronneai <zeronne.ai@gmail.com>
2026-01-22 11:50:54 -0600

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css' // Asegúrate de tener este archivo o quita esta línea
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
